c90a971d5e02efbc726557ca36e61c20632b1dfc
travis_bird <travis.bird@oracle.com>
2021-10-26 14:37:30 +0100

    add new command add_event and associated block

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def hello_world(ack, client, logger, body):
     ack()
     logger.info(body)
     user_id = body["user_id"]
-    channel_id = body["channel_id"]
+    #channel_id = body["channel_id"]
 
     client.chat_postMessage(
-        channel=channel_id,
+        channel=user_id,
         as_user=True,
         text="Hello world!")
 
@@ -37,6 +37,100 @@ def help(ack, logger, body, client):
         channel=user_id,
         as_user=True,
         text="I am here to help you"
+    )
+
+@app.command("/add_event")
+def add_event(ack, logger, body, client):
+    """ Suggests a new event """
+    ack()
+    logger.info(body)
+    user_id = body["user_id"]
+
+    client.chat_postMessage(
+        channel=user_id, 
+        as_user=True, 
+	blocks = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hello! There is a gap in your calendar at *13:00*, perhaps you would like *to go on a walk*"
+			}
+		},
+		{
+			"type": "input",
+			"element": {
+				"type": "timepicker",
+				"initial_time": "13:00",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select time",
+					"emoji": True
+				},
+				"action_id": "timepicker-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Start time",
+				"emoji": True
+			}
+		},
+		{
+			"type": "input",
+			"element": {
+				"type": "timepicker",
+				"initial_time": "13:30",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select time",
+					"emoji": True
+				},
+				"action_id": "timepicker-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": "Finish time",
+				"emoji": True
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Accept",
+						"emoji": True
+					},
+					"value": "click_me_123",
+					"action_id": "actionId-0",
+					"style": "primary"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Decline",
+						"emoji": True
+					},
+					"value": "Decline",
+					"action_id": "actionId-1",
+					"style": "danger"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Different activity!",
+						"emoji": True
+					},
+					"value": "Decline",
+					"action_id": "actionId-2"
+				}
+			]
+		}
+	]
     )
 
 # Start your app
